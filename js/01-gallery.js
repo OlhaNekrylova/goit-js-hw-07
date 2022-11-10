cf41ae4be6edf2cf6f243166226246fa82dff30e
OlhaNekrylova <nekrylovaurist@gmail.com>
2022-11-10 21:47:17 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const newGallery = document.querySelector('.gallery');
+const galleryMarkup = createGalLeryMarkup (galleryItems);
+
+function createGalLeryMarkup (galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+            <img
+            class="gallery__image"
+            src="small-image.jpg"
+            data-source="large-image.jpg"
+            alt="Image description"
+            />
+        </a>
+        </div>`
+    })
+    .join('');
+}
+newGallery.insertAdjacentHTML('beforeend', galleryMarkup);
+console.log(newGallery);
+
+newGallery.addEventListener('click', onNewGalleryClick);
+function onNewGalleryClick (evt) {
+    console.log(evt.target.original);
+    
+
+}
