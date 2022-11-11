ca1f5b037b6814dbd468b38982794ea2c7e11d00
OlhaNekrylova <nekrylovaurist@gmail.com>
2022-11-11 14:51:46 +0200

    js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,4 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
 
@@ -26,9 +25,48 @@ function createGalLeryMarkup (galleryItems) {
 newGallery.insertAdjacentHTML('beforeend', galleryMarkup);
 console.log(newGallery);
 
-newGallery.addEventListener('click', onNewGalleryClick);
-function onNewGalleryClick (evt) {
-    console.log(evt.target.original);
-    
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`);
 
+const object = {
+	/*
+	 * Prevents the lightbox from closing when clicking its background.
+	 */
+	closable: true,
+	/*
+	 * One or more space separated classes to be added to the basicLightbox element.
+	 */
+	className: 'modal',
+	/*
+	 * Function that gets executed before the lightbox will be shown.
+	 * Returning false will prevent the lightbox from showing.
+	 */
+	onShow: (instance) => {},
+	/*
+	 * Function that gets executed before the lightbox closes.
+	 * Returning false will prevent the lightbox from closing.
+	 */
+	onClose: (instance) => {}
 }
+
+newGallery.addEventListener('click', onNewGalleryClick, object);
+function onNewGalleryClick (event) {
+    console.log(evt.target.original);
+    event.preventDefault();
+    
+}
+
+instance.show();
+instance.close();
+
+document.body.addEventListener('keypress', (event) => {
+    if (event.code === "Escape") {
+        instance.close();
+    } 
+})
